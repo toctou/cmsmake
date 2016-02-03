6a3446446ade6427d6168fa9e46f72b790b99488
toctou <gio.bartolomucci@gmail.com>
2016-02-03 22:41:26 +0100

    Added installation scripts
    Added installation scripts and organized in subfolders

---
+++ scripts/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Preparing space..."
+
+rm -rf /etc/cmsmake
+mkdir /etc/cmsmake
+rm -f /usr/local/bin/cmsmk
+
+echo "Downloading files..."
+
+git clone -q https://github.com/toctou/cmsmake.git /etc/cmsmake
+
+echo "Setting up..."
+
+chmod +x /etc/cmsmake/scripts/cmsMakeStart.sh
+ln -s /etc/cmsmake/scripts/cmsMakeStart.sh /usr/local/bin/cmsmk
+
+echo "Cleaning up..."
+
+rm -f install.sh
+
+echo -e "DONE\n"
+echo -e "Invoke the tool cmsMake by typing 'cmsmk'\n"
